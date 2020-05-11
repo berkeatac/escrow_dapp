d864c2a1631b5135866548a5767911065b863a39
Oğuz Özsaygın <oguzozsaygin@sabanciuniv.edu>
2020-05-12 00:50:08 +0300

    Added error messages for require stmts and pushed methods which should be prev commit

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.6.4;
 
 
 contract Marketplace {
@@ -21,8 +21,8 @@ contract Marketplace {
     }
 
     function createItem(string memory _name, uint256 _price) public {
-        require(bytes(_name).length > 0);
-        require(_price > 0);
+        require(bytes(_name).length > 0, "");
+        require(_price > 0, "");
 
         itemCount++;
         items[itemCount] = Item(
@@ -34,5 +34,37 @@ contract Marketplace {
             false,
             false
         );
+    }
+
+    function purchaseProduct(uint256 _id) public payable {
+        // fetch the item
+        Item memory _item = items[_id];
+        require(
+            msg.value > _item.price,
+            "there must be enough ether on message sent"
+        );
+        require(_item.id > 0 && _item.id <= itemCount, "item must have a valid id");
+        require(!_item.purchased, "item does not have to be purchased before");
+        require(_item.owner != msg.sender, "seller cannot buy its own item");
+        _item.purchased = true;
+        _item.buyer = msg.sender;
+        items[_id] = _item;
+    }
+
+    function verifyPurchase(uint256 _id) public payable {
+        Item memory _item = items[_id];
+        require(msg.sender == _item.buyer, "msg.sender must be item buyer");
+        require(
+            _item.purchased && !_item.verified,
+            "item must be purchased but not verified"
+        );
+        require(_item.price > 0, "item price must be unsigned integer");
+        _item.purchased = true;
+        _item.verified = true;
+        address payable _seller = _item.owner;
+        _item.owner = msg.sender;
+        _item.buyer = address(0);
+        items[_id] = _item;
+        _seller.transfer(_item.price);
     }
 }
